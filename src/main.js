df5455cf3dab1fe8cdcd000b07a24bca90cd56cd
pambatista <pambatista93@gmail.com>
2020-08-04 19:54:08 -0300

    add images

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import DefaultLayout from "~/layouts/Default.vue";
 export default function(Vue, { router, head, isClient }) {
   head.link.push({
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Roboto"
+    href: "https://fonts.googleapis.com/css2?family=Roboto&family=Poppins"
   });
   // Set default layout as a global component
   Vue.component("Layout", DefaultLayout);
